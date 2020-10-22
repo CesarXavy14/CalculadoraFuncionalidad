4484953945ffca6856d7ca21179660a856e2b074
Cesar Hernandez <xesar14javier@hotmail.com>
2020-10-21 21:03:22 -0500

    Funcionalidad de la calculadora

--- app/src/main/java/com/judecx/calculadora/MainActivity.java
+++ app/src/main/java/com/judecx/calculadora/MainActivity.java
@@ -10,15 +10,77 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    TextView imprimir,numero;
+    String valor="",signo ="";
+    Double nUno = 0.0;
+    Double nDos = 0.0;
+    Double resultado = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imprimir = findViewById(R.id.imprimir);
     }
 
 
+    @SuppressLint("SetTextI18n")
+    public void teclado(View view){
+        numero = (TextView)view;
+        valor += numero.getText().toString();
+        if  (nUno == 0){
+            imprimir.setText(valor);
+        }else{
+            imprimir.setText(nUno.toString()+signo+valor);
+        }
+    }
 
+    @SuppressLint("SetTextI18n")
+    public  void operacion(View view){
+        nUno = Double.parseDouble(valor);
+        valor="";
+        switch (view.getId()){
+            case R.id.suma:
+                signo = "+";
+                break;
+            case R.id.resta:
+                signo = "-";
+                break;
+            case R.id.muliplicacion:
+                signo = "x";
+                break;
+            case R.id.division:
+                signo = "÷";
+                break;
+        }
+        imprimir.setText(nUno.toString()+signo);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void resultado(View view){
+        nDos = Double.parseDouble(valor);
+        switch (signo){
+            case "+":
+                resultado = nUno+nDos;
+                break;
+            case "-":
+                resultado = nUno-nDos;
+                break;
+            case "x":
+                resultado = nUno*nDos;
+                break;
+            case "÷":
+                resultado = nUno/nDos;
+                break;
+        }
+        imprimir.setText(resultado.toString());
+    }
+
+    public void eliminar(View view){
+        valor ="";
+        nUno = 0.0;
+        nDos = 0.0;
+        resultado = 0.0;
+        imprimir.setText("");
+    }
 }
